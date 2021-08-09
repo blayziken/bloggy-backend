00728cae77bb109629707828aa11f25be96de92e
blayziken <kingtoluwalope@gmail.com>
2021-08-09 10:57:45 +0100

    setting up mongoose

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('b');
+const path = require('path');
+const express = require('express');
+// const morgan = require('morgan');
+// const cookieParser = require('cookie-parser');
+
+// const tourRouter = require('./routes/tourRoutes');
+// const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+
+// 1) GLOBAL MIDDLEWARES
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Body Parser, reading data from the body into req.body
+// app.use(express.json());
+// app.use(cookieParser()); // middleware to parse cookie (*console.log(req.cookie))
+// app.use(express.urlencoded({ extended: true })); // middleware used to parse data coming from a FORM
+
+// 3) ROUTES
+// app.use('/', viewRouter);
+// app.use('/api/v1/tours', tourRouter);
+// app.use('/api/v1/users', userRouter);
+
+
+// app.all('*', (req, res, next) => {
+//     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+// })
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION, Shutting down!..')
+    console.log(err.name, err.message);
+
+    process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+const DB = 'mongodb://localhost:27017/natours'; // local DB connection
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 8000;
+const server = app.listen(port, () => {
+    console.log(`Server running on port 8000...`);
+});
+
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION, Shutting down!..')
+    console.log(err.name, err.message);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
