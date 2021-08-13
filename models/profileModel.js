af0693990bbc48e3c7d54c9d1e481de27a581789
blayziken <kingtoluwalope@gmail.com>
2021-08-13 14:51:16 +0100

    created profile model

---
+++ models/profileModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    name: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    profession: String,
+    DOB: String,
+    titleline: String,
+    about: String,
+    img: {
+        type: String,
+        default: ""
+    }
+
+},
+    {
+        timestamp: true,
+    }
+);
+
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
